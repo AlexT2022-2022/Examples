8127b45b1ed7cc624c186b80243ec60fd66e15a0
Alex <nordnord2234@gmail.com>
2023-01-08 04:09:32 +0300

    Add Example004 Div  деление  +

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿double numberA = 12; //еслм  int поменять на double, то можно делить с остатком\ при int  деление должно быть без остатка т.е. например 6/2
+double numberB = 5;
+Console. WriteLine (numberA / numberB);
