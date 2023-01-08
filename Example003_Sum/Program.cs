8ef5b4b7b48a470b58cbe4b4a4ccedc115dd337d
Alex <nordnord2234@gmail.com>
2023-01-08 04:41:21 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA =31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); //написали команду  new Random  т.е. 1 2 3 4 5 ,,,,, смотри commit по времени
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
